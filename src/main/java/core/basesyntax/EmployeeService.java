6b9c2ec75a994281b225f323133219cc14b633a2
Kamil Jan Mularski <kammul@outlook.com>
2025-09-26 12:01:37 +0200

    Implement employee sorting by age and name
    Added a custom comparator to sort employees first by age and then by name. The getEmployByOrder method now returns a sorted set of employees using this comparator.

--- src/main/java/core/basesyntax/EmployeeService.java
+++ src/main/java/core/basesyntax/EmployeeService.java
@@ -1,10 +1,21 @@
 package core.basesyntax;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class EmployeeService {
+    private static final Comparator<Employee> AGE_NAME_COMPARATOR = new Comparator<Employee>() {
+        @Override
+        public int compare(Employee first, Employee second) {
+            return first == second || first.equals(second) ? 0
+                    : first.getAge() > second.getAge() ? 1
+                    : first.getAge() < second.getAge() ? -1
+                    : first.getName() == null ? -1 : first.getName().compareTo(second.getName());
+        }
+    };
+
     public Set<Employee> getEmployByOrder(List<Employee> employees) {
-        return null;
+        SortedSet<Employee> employeeSortedSet = new TreeSet<>(AGE_NAME_COMPARATOR);
+        employeeSortedSet.addAll(employees);
+        return employeeSortedSet;
     }
 }
